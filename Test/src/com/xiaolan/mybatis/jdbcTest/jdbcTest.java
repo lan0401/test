10ac1764a56dff2b2bceac8d0e01d12aa48defa6
lan0401 <lanbujuexiao@gmail.com>
2019-09-10 09:35:06 +0800

    first version

---
+++ Test/src/com/xiaolan/mybatis/jdbcTest/jdbcTest.java
@@ -0,0 +1,43 @@
+package com.xiaolan.mybatis.jdbcTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class jdbcTest {
+	public static void main(String[] args) {
+		
+		Connection connection=null;
+		PreparedStatement prep=null;
+		ResultSet  resultset=null;
+		
+		try{
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			
+			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/mybatis?characterEncode=utf-8&serverTimezone=UTC","root","xiaolan");
+			
+			String sql="select * from user where username= ? ";
+			
+			prep=connection.prepareStatement(sql);
+			
+			prep.setString(1, "ÍõÎå");
+			
+			resultset=prep.executeQuery();
+			
+			
+			while(resultset.next()){
+				System.out.println(resultset.getString("id")+" "+resultset.getString("username"));
+			}
+			
+			
+		} catch (Exception e){
+			System.out.println(e);
+		}finally{
+			
+		}
+	}
+	
+}
